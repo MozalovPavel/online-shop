9cc2e1242bc998b7bb5b8fb73ddbc634583c7134
mozalov <pp-1995@mail.ru>
2022-11-15 23:44:00 +0300

    adds cart

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import styles from './Cart.module.css';
+
+import cartImage from '../../../public/icons/cart.svg';
+
+interface ICartProps {
+    ordersCount: number;
+    onClick: () => void;
+}
+
+const Cart = (props: ICartProps) => {
+    const {ordersCount, onClick} = props;
+    return (
+        <div className={styles.root} onClick={onClick}>
+            <Image src={cartImage} width={24} height={24} alt="Cart" />
+            {!!ordersCount && <span className={styles.ordersCount}>{ordersCount}</span>}
+        </div>
+    );
+};
+   
+   export default Cart;
